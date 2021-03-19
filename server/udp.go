43ab06386f39a552c51a9bb19fa339842ed8e3e3
XueHua Huang <xuehua.huang@dbjtech.com>
2021-03-19 13:51:40 +0800

    增加对显示屏终端的特殊处理

--- server/udp.go
+++ server/udp.go
@@ -1,8 +1,10 @@
 package server
 
 import (
+	"encoding/json"
 	"go-echo-server/datagram"
 	"net"
+	"strings"
 	"time"
 )
 
@@ -20,6 +22,39 @@ func process(buf []byte, addr *net.UDPAddr) datagram.Datagram {
 			if buf[i] == '#' {
 				projectName = string(buf[1:i])
 				content = buf[i+1:]
+			}
+		}
+	} else { // 对显示屏终端进行特殊处理
+		valueDotKey := strings.Split(string(buf), "=")
+		if len(valueDotKey) > 1 {
+			keys := make([]string, 0, len(valueDotKey))
+			values := make([]interface{}, 0, len(valueDotKey))
+
+			keys = append(keys, valueDotKey[0])
+			for i := 1; i < len(valueDotKey)-1; i++ {
+				entry := valueDotKey[i]
+				lastIndex := strings.LastIndex(entry, ",")
+				var v interface{} = entry[:lastIndex]
+				var mapResult map[string]interface{}
+				if err := json.Unmarshal([]byte(v.(string)), &mapResult); err == nil {
+					v = mapResult
+				}
+				k := entry[lastIndex+1:]
+				keys = append(keys, k)
+				values = append(values, v)
+			}
+			values = append(values, valueDotKey[len(valueDotKey)-1])
+
+			jsonMap := make(map[string]interface{})
+			for i := 0; i < len(keys)-1; i++ {
+				if keys[i] == "project" {
+					projectName = values[i].(string)
+				} else {
+					jsonMap[keys[i]] = values[i]
+				}
+			}
+			if b, err := json.Marshal(jsonMap); err == nil {
+				content = b
 			}
 		}
 	}
